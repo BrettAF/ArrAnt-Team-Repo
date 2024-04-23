bfc9ce99c5ff8bd2d9f83b20c0865e5c2d224694
BrettAF <bford5@uccs.edu>
2024-04-23 10:46:40 -0600

    Final version of code
    This is the version that is in the final report.

--- loop_gain_function.m
+++ loop_gain_function.m
@@ -1,23 +1,23 @@
-function loop_gain = loop_gain_function(number, diameters, power,receivers, transmitters, k,lambda)
+function loop_gain = loop_gain_function(quantity,diameters,power,receivers,transmitters,k,lambda)
     % This function calculates loop gain.
     %
-    % number: a vector of the quantity of each type of antenna
+    % quantity: a vector of the quantity of each type of antenna
     % diameters: a vector of the diameters of each type of antenna
-    % power: Transmitter power recieved as a vector
-    % recievers: a boolean vector with 1 for a reciever and 0 for a
+    % power: Transmitter power received as a vector
+    % recievers: a Boolean vector with 1 for a reciever and 0 for a
     %   transmitter
-    % transmitters: a boolean vector with 1 for transmitters and 0 for
+    % transmitters: a Boolean vector with 1 for transmitters and 0 for
     %   recievers
     % k: efficiency of the antenna
     % lambda: wavelength as GHz/10cm
     
     % num_transmitters: A vector of the number of transmitters with 0 for
-    %   the recievers
-    num_transmitters = number.*transmitters;
+    %   the receivers
+    num_transmitters = quantity.*transmitters;
     
     % num_recievers: A vector of the number of recievers in each style with
     %   0 for transmitters
-    num_recievers = number.*receivers;
+    num_receivers = quantity.*receivers;
     
     % power_transmitters: Transmitter power recieved as a vector with 0 for
     %   recievers
@@ -26,44 +26,38 @@ function loop_gain = loop_gain_function(number, diameters, power,receivers, tran
     % EIRP of the array
     EIRP = gain_all_transmitters(k,num_transmitters,diameters,lambda,power_transmitters);
     
-    % MGr : total gain of the recievers in the array
-    MGr = gain_all_recievers(k,num_recievers,diameters,lambda);
+    % array_gain_receiver : total gain of the recievers in the array
+    array_gain_receiver = gain_all_receivers(k,num_receivers,diameters,lambda);
 
     % Loop Gain Equation
-    % eirp_a = NPt * NGt;
-    % MGr = M * Gr
-    % loop_gain = eirp of the array + array gain of recievers;
-    % loop_gain = EIRP + MGr
-    loop_gain = EIRP + MGr;
+    % EIRP of the array = (number of transmitters)(power of transmitters) * (number of tramsitters)(gain of transmitters)
+    % array_gain_receiver = number of recievers * gain of receivers
+    % loop_gain = eirp of the array + gain of receivers;
+    % loop_gain = EIRP + array_gain_receiver
+    loop_gain = EIRP + array_gain_receiver;
 
-
-
-    function EIRP_A_dB = gain_all_transmitters(k,num_transmitters,diameters,lambda,power) % EIRP
+    function eirp_array_dB = gain_all_transmitters(k,num_transmitters,diameters,lambda,power) % EIRP
         % Calculate transmitter gain for the array
         % This function calculates parabolic gain.
         % 
         % diameter: Diameter of the antennas, recieved as a vector
         % lambda: Wavelength of the signal
-        % power: power to the arrays in watts, an array with 0 for
-        %   recievers
+        % power: power to the arrays in watts, an array with 0 for recievers
         % k: effencency of the arrays
 
         % returns the parabolic array of one antenna of each style of the
         % radar array, returns an vector
-        G_t = k * (pi * (diameters ./ lambda)).^2; 
+        gain_transmitter = k * (pi * (diameters ./ lambda)).^2; 
         
         % returns the sum gain of all of the antennas of each style of the array
-        %array_gain_t = num_transmitters .* parabolic_gain;
-        %^^^ I think the issue is here. EIRP of the transmit is
-        %log10(N^2*P*G), for each individual style, then summed together
+        %array_gain_transmitter = num_transmitters .* parabolic_gain;
         
-        array_gain_t = num_transmitters.^2.*power.*G_t;
-        %correct form
-        total_gain=sum(array_gain_t);
-        EIRP_A_dB = 10 * log10(total_gain);
+        array_gain_transmitter = num_transmitters.^2.*power.*gain_transmitter;
+        total_gain_transmitter=sum(array_gain_transmitter);
+        eirp_array_dB = 10 * log10(total_gain_transmitter);
     end
 
-    function reciever_gain_dB = gain_all_recievers(k,num_recievers,diameters,lambda) %MGr
+    function receiver_gain_dB = gain_all_receivers(k,num_receivers,diameters,lambda) 
         % Calculate total reciever gain for the array
         % This function calculates parabolic gain.
         % diameter: Diameter of the antennas, recieved as a vector
@@ -71,20 +65,17 @@ function loop_gain = loop_gain_function(number, diameters, power,receivers, tran
 
         % returns the parabolic array of one antenna of each style of the
         % radar array, returns an vector
-        G_r = k * (pi * (diameters ./ lambda)).^2;
+        gain_receiver = k * (pi * (diameters ./ lambda)).^2;
        
         % returns the gain of all of the antennas of each style of the array
-        array_gain_r = num_recievers .* G_r;
-        total_gain=sum(array_gain_r);
-        reciever_gain_dB = 10 * log10(total_gain);
+        array_gain_receiver = num_receivers .* gain_receiver;
+        total_gain_receiver=sum(array_gain_receiver);
+        receiver_gain_dB = 10 * log10(total_gain_receiver);
     end
 
 end
 
 
-
-
 % an example for running the loop_gain_function in the command line:
-% loop_gain_function(number, diameters, power,recievers, transmitters, k,lambda)
+% loop_gain_function(number, diameters, power,receivers, transmitters, k,lambda)
 % lg = loop_gain_function([2,3], [30,40], [1000,9000],[0,1], [1,0], .6,.1)
-
--- optimization_output.m
+++ optimization_output.m
@@ -1,166 +1,171 @@
-% Optimization_output script that displays a table and graph of optimal
-% solutions
-%      
+function optimization_output(x,num_styles,operative_values, include_monostatic,loop_gain_desired,min_t_styles,min_r_styles,min_quantity,max_quantity,min_diameter,max_diameter,min_power,max_power,year_built,k,nu,max_antennas)
 
-function table = optimization_output(x,styles,operative_values, include_monostatic,desired_loop_gain)
+    function disp_table = create_table(x,num_styles,operative_values,include_monostatic)
+         % Number of Parameters (type, quantity, diameter, power)
+         num_parameters = 4;
 
-    include_monostatic
+         % Create the Column Names
+         col_names = [];
+         for i = 1:num_styles
+             col_names = [col_names,"Type "+i,"Quantity "+i, "Diameter "+i+ " (m)","Power "+i + " (W)"];
+         end 
+         
+         col_names = [col_names,"Cost ($M)","Gain (dB,dBW)"];
 
-     % create column names
-     col_names = [];
-     for i = 1:styles
-         col_names = [col_names,"Type "+i,"Quantity "+i, "Diameter "+i+ " (m)","Power "+i + " (W)"];
-     end % end for loop
- 
- % These two lines add the cost and gain values to the table
- col_names = [col_names,"Cost ($M)","Gain (dB)"];
- x(:,(4*styles+1):(4*styles+2)) = operative_values;
- x(:,(4*styles+2)) = -x(:,(4*styles+2));
- 
- % create a table using the x matrix and column names
- table = array2table(x,'VariableNames',col_names);
+         % Add the cost and gain objective values to the x matrix
+         x(:,(num_parameters*num_styles+1):(num_parameters*num_styles+2)) = operative_values;
+         x(:,(num_parameters*num_styles+2)) = -x(:,(num_parameters*num_styles+2));
+       
+        
+         % create a table using the x matrix and column names
+         table = array2table(x,'VariableNames',col_names);
+                   
+         % sort the table based on the gain column
+         table = sortrows(table,'Gain (dB,dBW)');
+        
+         % change cost column to be in millions
+         table.("Cost ($M)") = round(table.("Cost ($M)")/ 1e6, 3);
+         
+         % dynamically adjust values in the table 
+         for i = 1:(num_styles)
+        
+             % change type columns to have have 0, 1 or 2 values (instead of float)
+             if strcmp(include_monostatic,"T")
+                 % Transmitters - 0
+                 % Monostatic - 1
+                 % Receivers - 2
+                 table.(num_parameters*i-3) = floor(table.(num_parameters*i-3)); % Type
+             else
+                 % Transmitter < 1.5
+                 % Reciever >= 1.5
+                 %Logical indexing to change values
+                 table.(num_parameters*i-3)(table.(num_parameters*i-3) >= 1.5) = 2;  % if greater than or equal to 1.5 it is a reciever
+                 table.(num_parameters*i-3)(table.(num_parameters*i-3) < 1.5) = 0; % if less than 1.5 it is considered a transmitter
+             end % end if/else strcmp
+             
+             % change quantity columns to integer (instead of float)
+             table.(num_parameters*i-2) = floor(table.(num_parameters*i-2)); % quantity
+        
+             % change reciever power to 0
+             table.(num_parameters*i)(table.(num_parameters*i-3)==2)=0; % receiver
+        
+             % change type column to have text instead of integer   
+             col_name_type = "Type " + string(i); 
+             table.(col_name_type) = floor(table.(col_name_type));
+             table.(col_name_type) = arrayfun(@(int) say_type(int), table.(col_name_type), 'UniformOutput', false);
+        
+              numCols = width(table);
+        
+            % Move the Gain and Cost values to the beginning of the table
+            disp_table = [table(:, numCols), table(:, numCols-1), table(:, 1:numCols-2)];
+         end % end for i = 1:styles
+ % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        % say_type Function Definition
+        % inputs an integer representation of antennae and outputs a text 
+        % representation of antennae 
+        % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        % Parameters
+        % t = the integer representation of antennae (0, 1 or 2)
+        % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        % Return
+        % type = text representation of antennae (Transmitter, Receiver,
+        % Monostatic)
+        % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   
 
- % sort the table based on the gain column
- table = sortrows(table,'Gain (dB)');
+       function type_string = say_type(type_int)
+            type_int = floor(type_int);
+            if type_int == 0
+                type_string='Transmitter';
+            elseif type_int== 1
+                type_string='Monostatic';
+            elseif type_int== 2
+                type_string='Receiver';
+            else 
+                type_string='error';
+            end
+         end % end say_type function
+    end
 
- % change cost column to be in millions
- table.("Cost ($M)") = round(table.("Cost ($M)")/ 1e6, 3);
- 
- % dynamically adjust values in the table 
- for i = 1:(styles)
-
-     % change type columns to have have 0, 1 or 2 values (instead of float)
-     if strcmp(include_monostatic,"T")
-         % Transmitters - 0
-         % Monostatic - 1
-         % Receivers - 2
-         table.(4*i-3) = floor(table.(4*i-3)); % Type
-     else
-         % Transmitter < 1.5
-         % Reciever >= 1.5
-         %Logical indexing to change values
-         table.(4*i-3)(table.(4*i-3) >= 1.5) = 2;  % if greater than or equal to 1.5 it is a reciever
-         table.(4*i-3)(table.(4*i-3) < 1.5) = 0; % if less than 1.5 it is considered a transmitter
-     end % end if/else strcmp
+       
      
-     % change quantity columns to integer (instead of float)
-     table.(4*i-2) = floor(table.(4*i-2)); % quantity
-
-     % change reciever power to 0
-     table.(4*i)(table.(4*i-3)==2)=0; % receiver
-
-     % change type column to have text instead of integer   
-     colName = "Type " + string(i); 
-     table.(colName) = floor(table.(colName));
-     table.(colName) = arrayfun(@(x) say_type(x), table.(colName), 'UniformOutput', false); % converts column to a string 
-
- end % end for i = 1:styles
-
 
     % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    % say_type Function Definition
-    % inputs an integer representation of antennae and outputs a text 
-    % representation of antennae 
+    % output_graph Function Definition
+    % create a scatterplot to represent cost and gain optimized solutions
     % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     % Parameters
-    % t = the integer representation of antennae (0, 1 or 2)
+    % table = table of optimized solution
+    % desired_loop_gain: input from user in main_life_script to determine
+    % the upper and lower horizontal lines.
     % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     % Return
-    % type = text representation of antennae (Transmitter, Receiver,
-    % Monostatic)
+    % displayed scatterplot graph
     % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
- function type = say_type(t)
-    t = floor(t);
-    if t == 0
-        type='Transmitter';
-    elseif t== 1
-        type='Monostatic';
-    elseif t== 2
-        type='Receiver';
-    else 
-        type='error';
-    end
- end % end say_type function
- 
 
-
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% output_graph Function Definition
-% create a scatterplot to represent cost and gain optimized solutions
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% Parameters
-% table = table of optimized solution
-% desired_loop_gain: input from user in main_life_script to determine
-% the upper and lower horizontal lines.
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% Return
-% displayed scatterplot graph
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-function t_output_graph(table,desired_loop_gain)
-    
-    % variables to hold the upper and lower line values 
-    upper_line = desired_loop_gain*1.1;
-    lower_line = desired_loop_gain*.9;    
-
-    % Create scatter of table.Cost and table.Gain
-    s = scatter(table,"Cost ($M)","Gain (dB)");
-    
-    % Add xlabel, ylabel, title, legend and lower and upper gain lines
-    xlabel("Cost ($M)")
-    ylabel("Gain (dB)")
-    title("Gain vs. Cost")
-    yline(upper_line,'-.g')
-    yline(lower_line,'-.g')
-
-    % Customizing data tips
-    dcm = datacursormode(gcf); % Get the data cursor mode object for the current figure
-    set(dcm, 'UpdateFcn', @(obj,event_obj) customDataTip(obj, event_obj, table));
-
-    
-end % t_output_graph function
-
-% create a data tip so user can click on a data point and get more
-% information
-function txt = customDataTip(~, event_obj, table)
-
-    % Get the position of the clicked point
-    pos = event_obj.Position; 
-
-    % Find the index (row number) of the closest point based on Euclidean distance
-    distances = hypot(table.("Cost ($)") - pos(1), table.("Gain (dB)") - pos(2));
-    [~, row] = min(distances);
-
-    % Create the data tip text showing the row number
-    txt = {['Solution: ', num2str(row)], ...
-            ['Cost ($M)', num2str(table.("Cost ($M)")(row))], ...
-            ['Gain: ', num2str(table.("Gain (dB)")(row)), ' dB']};
-end % end custumDataTip
-
-function saveCSV(parameters, table)
-
-        labels = ["k";"Lambda";"number_of_styles"; "minimum_recievers_styles"; "minimum_transmitter_styles"; "min_diameter";"max_diameter";
-            "min_quantity" ;"max_quantity";"min_power" ;"max_power"; "maximum_elements"; "loop_gain_desired"; "year";"include_monostatic"];
+    function create_graph(disp_table,loop_gain_desired)
         
-        parameters = parameters.';
-        dictionary = array2table(parameters, 'RowNames', labels);
-        %dictionary = table(horzcat(labels, parameters));
-        t = datetime('now','TimeZone','local','Format','d_MMM_y_HH_mm_ss');
-        t=string(t);
-        filename = strcat("arraySolutions_",t,".xlsx");
-
-        % Write the dictionary to the 'Parameters' sheet
-        writetable(dictionary, filename, 'WriteRowNames', true, 'Sheet', 'Input Parameters');
+        % variables to hold the upper and lower line values 
+        upper_line = loop_gain_desired*1.1;
+        lower_line = loop_gain_desired*.9;    
     
-        % Write the provided table to the 'Output Table' sheet
-        writetable(table, filename, 'Sheet', 'Output Table');
+        % Create scatter of table.Cost and table.Gain
+        scatter_graph = scatter(disp_table,"Cost ($M)","Gain (dB,dBW)");
+        
+        % Add xlabel, ylabel, title, legend and lower and upper gain lines
+        xlabel("Cost ($M)")
+        ylabel("Gain (dB,dBW)")
+        title("Gain vs. Cost")
+        yline(upper_line,'-.g')
+        yline(lower_line,'-.g')
+    
+    % Customizing data tips
+        dcm = datacursormode(gcf);
+        set(dcm, 'UpdateFcn', @(src, event_obj) customDataTip(event_obj, disp_table));
+        datacursormode on;  % Turn on the data cursor mode
+    
+        % Inner function for custom data tip text
+        function txt = customDataTip(event_obj, disp_table)
+            pos = event_obj.Position;
+    
+            % Find the index (row number) of the closest point
+            distances = hypot(disp_table.("Cost ($M)") - pos(1), disp_table.("Gain (dB,dBW)") - pos(2));
+            [~, row] = min(distances);
+    
+            % Create the data tip text
+            txt = {['Solution: ', num2str(row)], ...
+                   ['Cost ($M)', num2str(disp_table.("Cost ($M)")(row))], ...
+                   ['Gain: ', num2str(disp_table.("Gain (dB,dBW)")(row)), ' dB, dBW']};
+        end
+            
+        end % t_output_graph function
+
+        function saveFile(disp_table)
+
+            list_parameter_names = ["Number of Styles"; "Minimum Transmitter Styles"; "Minimum Recievers Styles";"Minimum Quantity";"Maximum Quantity";"Minimum Diameter";"Max Diameter"; "Minimum Power" ;"Maximum Power"; "Year to be Built"; "Include Monostatic"; "k"; "nu"; "Maximum Number of Antennas"; "Desired Loop Gain"];            
+            list_parameters = [num_styles;min_t_styles;min_r_styles;min_quantity;max_quantity;min_diameter;max_diameter;min_power;max_power;year_built;include_monostatic;k;nu;max_antennas;loop_gain_desired];
+            list_parameter_units = ["";"";"";"";"";"m";"m";"W";"W";"";"";"";"GHz";"";"dB, dBW"];
+
+            % Create the table
+            param_table = table(list_parameter_names, list_parameters, list_parameter_units, 'VariableNames', {'Parameter', 'Value', 'Unit'});
+            
+            current_time = datetime('now','TimeZone','local','Format','y_MMM_d_HH_mm_ss');
+            current_time = string(current_time);
+            filename = strcat("arraySolutions_",current_time,".xlsx");
+    
+            % Write the dictionary to the 'Parameters' sheet
+            writetable(param_table, filename, 'WriteRowNames', true, 'Sheet', 'Input Parameters');
+        
+            % Write the provided table to the 'Output Table' sheet
+            writetable(disp_table, filename, 'Sheet', 'Output Table');
  
-end % end of saveCSV
-
+        end
 % display output graph
-t_output_graph(table,desired_loop_gain)
+disp_table = create_table(x,num_styles,operative_values, include_monostatic)
+create_graph(disp_table,loop_gain_desired)
+saveFile(disp_table)
 
-end % function table = optimization_output(x,styles,operative_values, include_monostatic, desired_loop_gain)
+end % function optimization_output(x,styles,operative_values, include_monostatic, desired_loop_gain)
+
+
